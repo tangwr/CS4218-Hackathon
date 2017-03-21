5d956f0840d150edee188f7a88afd463ef38e2f1
wireless90 <wireless-@hotmail.com>
2017-03-21 22:26:58 +0800

    more bugs

---
+++ Team_K/BugTest/TestBugEcho.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+import sg.edu.nus.comp.cs4218.exception.ShellException;
+import sg.edu.nus.comp.cs4218.impl.app.EchoApplication;
+import sg.edu.nus.comp.cs4218.impl.ShellImplementation;
+
+public class TestBugEcho {
+
+	EchoApplication echoApp;
+	ShellImplementation si;
+	@Before
+	public void setup(){
+		echoApp = new EchoApplication();
+		si = new ShellImplementation();
+	}
+
+	@Test
+	public void echoGlobbing() throws AbstractApplicationException, ShellException
+	{
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+		si.parseAndEvaluate("echo r*", output);
+		
+		String expected = "r*" + System.lineSeparator();
+		String actual = output.toString();
+		
+		assertEquals(expected, actual);
+	}
+}
